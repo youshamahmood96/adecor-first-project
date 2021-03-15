8c7e8511565b234732517f819b5605cc4843d40e
youshamahmood <youshamahmood96@gmail.com>
2021-03-16 03:06:56 +0600

    mobile changes

--- components/Landing/ScrollSnap.js
+++ components/Landing/ScrollSnap.js
@@ -236,6 +236,11 @@ const ScrollSnap = () => {
         </div>
         </div>
         </div>
+        <div className={styles.here} >
+        <div className="center-global" >
+        <div id="cbox-5xY3X9avT56K4n0T"></div>
+        </div>
+        </div>
 
 
 
